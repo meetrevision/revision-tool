6f458ef7471d719790f98b82099981c9b2252ae0
Felipe Eliseu <ownfeelipe@proton.me>
2022-11-25 19:27:28 -0300

    hide usability > power mode

--- lib/screens/pages/usability_page.dart
+++ lib/screens/pages/usability_page.dart
@@ -207,65 +207,65 @@ class _UsabilityPageState extends State<UsabilityPage> {
             ],
           ),
         ),
-        //
-        const SizedBox(height: 5.0),
-        CardHighlight(
-
-          child: Row(
-            children: [
-              const SizedBox(width: 5.0),
-              const Icon(
-                FluentIcons.lightbulb,
-                size: 24,
-              ),
-              const SizedBox(width: 15.0),
-              Expanded(
-                child: SizedBox(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      InfoLabel(label: 'Power Mode'),
-                      Text(
-                        "Customized powerplans to improve system latency.",
-                        style: FluentTheme.of(context).brightness.isDark
-                            ? const TextStyle(fontSize: 11, color: Color.fromARGB(255, 200, 200, 200), overflow: TextOverflow.fade)
-                            : const TextStyle(fontSize: 11, color: Color.fromARGB(255, 117, 117, 117), overflow: TextOverflow.fade),
-                      )
-                    ],
+        if (expBool) ...[
+          const SizedBox(height: 5.0),
+          CardHighlight(
+            child: Row(
+              children: [
+                const SizedBox(width: 5.0),
+                const Icon(
+                  FluentIcons.lightbulb,
+                  size: 24,
+                ),
+                const SizedBox(width: 15.0),
+                Expanded(
+                  child: SizedBox(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        InfoLabel(label: 'Power Mode'),
+                        Text(
+                          "Customized powerplans to improve system latency.",
+                          style: FluentTheme.of(context).brightness.isDark
+                              ? const TextStyle(fontSize: 11, color: Color.fromARGB(255, 200, 200, 200), overflow: TextOverflow.fade)
+                              : const TextStyle(fontSize: 11, color: Color.fromARGB(255, 117, 117, 117), overflow: TextOverflow.fade),
+                        )
+                      ],
+                    ),
                   ),
                 ),
-              ),
-              ComboBox(
-                value: powerPlan,
-                items: [
-                  ComboBoxItem(
-                    value: "3ff9831b-6f80-4830-8178-736cd4229e7b",
-                    child: Text("Ultra Performance"),
-                    onTap: () => setState(() {
-                      //
-                    }),
-                  ),
-                  ComboBoxItem(
-                    value: "e19c287e-faa8-494f-adf0-d8ed5ee4eef1",
-                    child: Text("Ultimate Performance"),
-                    onTap: () => setState(() {
-                      powerPlan = "3ff9831b-6f80-4830-8178-736cd4229e7b";
-                    }),
-                  ),
-                ],
-                onChanged: (value) {
-                /* Error 0x80070005: Access is denied.
-                  if (powerPlan == "3ff9831b-6f80-4830-8178-736cd4229e7b") {
-                    writeRegistryString(Registry.localMachine, r'SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes', 'ActivePowerScheme', "3ff9831b-6f80-4830-8178-736cd4229e7b");
-                  } else {
-                    writeRegistryString(Registry.localMachine, r'SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes', 'ActivePowerScheme', "e19c287e-faa8-494f-adf0-d8ed5ee4eef1");
-                  }
-                */
-                },
-              ),
-            ],
+                ComboBox(
+                  value: powerPlan,
+                  items: [
+                    ComboBoxItem(
+                      value: "3ff9831b-6f80-4830-8178-736cd4229e7b",
+                      child: Text("Ultra Performance"),
+                      onTap: () => setState(() {
+                        //
+                      }),
+                    ),
+                    ComboBoxItem(
+                      value: "e19c287e-faa8-494f-adf0-d8ed5ee4eef1",
+                      child: Text("Ultimate Performance"),
+                      onTap: () => setState(() {
+                        powerPlan = "3ff9831b-6f80-4830-8178-736cd4229e7b";
+                      }),
+                    ),
+                  ],
+                  onChanged: (value) {
+                  /* Error 0x80070005: Access is denied.
+                    if (powerPlan == "3ff9831b-6f80-4830-8178-736cd4229e7b") {
+                      writeRegistryString(Registry.localMachine, r'SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes', 'ActivePowerScheme', "3ff9831b-6f80-4830-8178-736cd4229e7b");
+                    } else {
+                      writeRegistryString(Registry.localMachine, r'SYSTEM\CurrentControlSet\Control\Power\User\PowerSchemes', 'ActivePowerScheme', "e19c287e-faa8-494f-adf0-d8ed5ee4eef1");
+                    }
+                  */
+                  },
+                ),
+              ],
+            ),
           ),
-        ),
+        ],
       ],
     );
   }
