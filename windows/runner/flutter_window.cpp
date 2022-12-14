c6217b793b4d6df71b5a1aea6fdc27f810053b17
Edgars Cīrulis <edgarsscirulis@gmail.com>
2022-12-14 14:08:20 +0200

    flutter_window: Remove unused std::optional
    The std::optional in the MessageHandler function is not used, so it can be removed.

--- windows/runner/flutter_window.cpp
+++ windows/runner/flutter_window.cpp
@@ -43,11 +43,12 @@ FlutterWindow::MessageHandler(HWND hwnd, UINT const message,
                               LPARAM const lparam) noexcept {
   // Give Flutter, including plugins, an opportunity to handle window messages.
   if (flutter_controller_) {
-    std::optional<LRESULT> result =
+    bool handled = false;
+    LRESULT result =
         flutter_controller_->HandleTopLevelWindowProc(hwnd, message, wparam,
-                                                      lparam);
-    if (result) {
-      return *result;
+                                                      lparam, &handled);
+    if (handled) {
+      return result;
     }
   }
 
