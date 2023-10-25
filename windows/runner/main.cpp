66e9df111929bdf1ce7a6062e67f46a4033d81c2
h3r0 <50238852+melo936@users.noreply.github.com>
2023-10-25 20:09:59 +0000

    fix the log reader stopped error

--- windows/runner/main.cpp
+++ windows/runner/main.cpp
@@ -9,7 +9,7 @@
 int APIENTRY wWinMain(_In_ HINSTANCE instance, _In_opt_ HINSTANCE prev,
                       _In_ wchar_t *command_line, _In_ int show_command) {
   
-  ::WindowPlusPluginCApiHandleSingleInstance(NULL, NULL);
+  // ::WindowPlusPluginCApiHandleSingleInstance(NULL, NULL);
   // Attach to console when present (e.g., 'flutter run') or create a
   // new console when running with a debugger.
   if (!::AttachConsole(ATTACH_PARENT_PROCESS) && ::IsDebuggerPresent()) {
