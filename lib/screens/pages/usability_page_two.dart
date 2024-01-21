c0def557f688c57c0b028165dbc5e45fea51f57b
h3r0 <50238852+melo936@users.noreply.github.com>
2024-01-21 15:55:03 +0000

    remove File Explorer Tabs #66

--- lib/screens/pages/usability_page_two.dart
+++ lib/screens/pages/usability_page_two.dart
@@ -15,13 +15,10 @@ class _UsabilityPageTwoState extends State<UsabilityPageTwo> {
   final UsabilityService _usabilityService = UsabilityService();
   late final _mrcBool =
       ValueNotifier<bool>(_usabilityService.statusNewContextMenu);
-  late final _tabsUWPbool =
-      ValueNotifier<bool>(_usabilityService.statusTabsUWP);
 
   @override
   void dispose() {
     _mrcBool.dispose();
-    _tabsUWPbool.dispose();
     super.dispose();
   }
 
@@ -42,17 +39,6 @@ class _UsabilityPageTwoState extends State<UsabilityPageTwo> {
             _mrcBool.value
                 ? await _usabilityService.enableNewContextMenu()
                 : await _usabilityService.disableNewContextMenu();
-          },
-        ),
-        CardHighlightSwitch(
-          icon: msicons.FluentIcons.folder_multiple_16_regular,
-          label: ReviLocalizations.of(context).usability11FETLabel,
-          switchBool: _tabsUWPbool,
-          function: (value) {
-            _tabsUWPbool.value = value;
-            _tabsUWPbool.value
-                ? _usabilityService.enableTabsUWP()
-                : _usabilityService.disableTabsUWP();
           },
         ),
       ],
