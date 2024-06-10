894cc88d3b3a260c6037d200395ca1a8999b3cd4
melo936 <50238852+melo936@users.noreply.github.com>
2024-06-10 17:08:55 +0400

    hide onedrive folder in explorer when installing system components removal package

--- lib/services/win_package_service.dart
+++ lib/services/win_package_service.dart
@@ -79,6 +79,15 @@ class WinPackageService {
   }
 
   Future<void> installPackage(final WinPackageType packageType) async {
+    if (packageType == WinPackageType.systemComponentsRemoval) {
+      // remove 'OneDrive' from explorer TODO: Remove this section after a new PB is released
+      RegistryUtilsService.writeDword(
+          Registry.classesRoot,
+          r'CLSID\{018D5C66-4533-4307-9B53-224DE2ED1FE6}',
+          'System.IsPinnedToNameSpaceTree',
+          0);
+    }
+
     if (!await File("$directoryExe\\cab-installer.ps1").exists()) {
       throw 'cab-installer.ps1 not found in $directoryExe. Please ensure the file is present by reinstalling Revision Tool.';
     }
