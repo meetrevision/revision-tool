f0e5feeaa6bbd40d833fa7d2beb105ec3b803a7c
h3r0 <50238852+melo936@users.noreply.github.com>
2025-10-05 20:31:29 +0400

    playbook_parches: disable webview2 spawning from SearchHost

--- src/lib/core/miscellaneous/playbook_patches_command.dart
+++ src/lib/core/miscellaneous/playbook_patches_command.dart
@@ -2,7 +2,9 @@ import 'dart:async';
 import 'dart:io';
 
 import 'package:args/command_runner.dart';
+import 'package:revitool/shared/win_registry_service.dart';
 import 'package:revitool/utils.dart';
+import 'package:win32_registry/win32_registry.dart';
 
 class PlaybookPatchesCommand extends Command<String> {
   static const tag = "Playbook Patches";
@@ -32,5 +34,15 @@ class PlaybookPatchesCommand extends Command<String> {
     exit(0);
   }
 
-  Future<void> applyPatches() async {}
+  Future<void> applyPatches() async {
+    logger.i('$name: Applying patches...');
+    // Disable WebView2 spawning from SearchHost
+    // https://www.reddit.com/r/WindowsHelp/comments/1lfu325/comment/n2nk50h
+    WinRegistryService.writeRegistryValue(
+      Registry.localMachine,
+      r'SYSTEM\ControlSet001\Control\FeatureManagement\Overrides\8\1694661260',
+      "EnabledState",
+      1,
+    );
+  }
 }
